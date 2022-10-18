b59db7eea29ea3057cbd8e548ff13d000b6dcbf0
Benjamin Babangile <benjaminbabangile@gmail.com>
2022-10-17 19:29:11 -0700

    print numbers from0 to 9 of base 10 without use of char

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - print numbers from zero to nine of base 10 without use of char
- *
- * Return: 0
+ * main - Prints all single digit numbers of base 10 starting from 0,
+ *        only using putchar and without char variables.
+ * Return: Always 0.
  */
 
 int main(void)
-{
-	int i;
 
+{
+	int num;
 	{
-	for (i = '0'; i = '9'; i++)
-		putchar(i);
+		for (num = 0; num < 10; num++)
+			putchar((num % 10) + '0');
 	}
 
 	putchar('\n');
